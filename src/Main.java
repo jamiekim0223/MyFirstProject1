7ea139c1fe67afda4994325de153d1cadd157443
JAMIE KIM <100169684@lubbockisd.net>
2022-08-31 10:12:36 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("My name is Dell!");
+        System.out.println("I am bad at coding.");
+        System.out.println("My duck is a cow.");
+    }
+}
